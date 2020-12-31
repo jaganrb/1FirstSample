dafd47d57bf18c2a44b619c13df105e2a8acc3c2
RJAGAN JAVA <rjagan.java@gmail.com>
2020-12-31 10:52:13 +0530

    Updated code

--- src/main/java/com/example/jagan/controller/SampleController.java
+++ src/main/java/com/example/jagan/controller/SampleController.java
@@ -2,12 +2,15 @@ package com.example.jagan.controller;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jagan.bean.Sample;
 import com.example.jagan.service.SampleService;
+
 
 @RestController
 public class SampleController {
@@ -16,22 +19,25 @@ public class SampleController {
 	private SampleService service;
 	
 	@GetMapping(path = "/")
-	public HashMap<String, String> imUpAndRunning() {
+	public HashMap<String, String> imUpAndRunning(HttpServletRequest request) {
 		if(service.count()==null) {
 			HashMap<String, String> returnMap = new HashMap<>();
 			returnMap.put("healthy", "false");
+			returnMap.put("IP", getClientIp(request));
 			return returnMap;
 		}else
 		{
 			HashMap<String, String> returnMap = new HashMap<>();
 			returnMap.put("healthy", "true");
 			returnMap.put("Count", service.count().size()+"");
+			returnMap.put("IP", getClientIp(request));
 			return returnMap;
 		}
 	}
 
 	@GetMapping(path = "/hit")
-	public HashMap<String, String> hitDb() {
+	public HashMap<String, String> hitDb(HttpServletRequest request) {
+		
 		Sample insert = new Sample();
 		insert.setInformation("Data");
 		service.save(insert);
@@ -41,4 +47,17 @@ public class SampleController {
 		return returnMap;
 	}
 	
+	private static String getClientIp(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
+    }
 }
